a16789c0f9f515ea3a2d13603640db1a6a4114ae
lucasSCsantos <luc.cristovam10@gmail.com>
2024-04-06 16:45:24 -0300

    push

--- src/entities/Square.java
+++ src/entities/Square.java
@@ -5,7 +5,7 @@ public class Square {
 	private int column, row;
 	private boolean isEmpty;
 	private Piece piece;
-	private char black = '■', white = '□';
+	private char black = '-', white = '-';
 
 	public Square(Colors color) {
 		this.color = color;
--- src/entities/Turn.java
+++
@@ -1,5 +0,0 @@
-package entities;
-
-public enum Turn {
-	PLAYER1, PLAYER2
-}
